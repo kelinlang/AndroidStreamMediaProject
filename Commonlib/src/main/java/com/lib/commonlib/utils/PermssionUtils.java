8415db453c2312cbd4b6d840b4319ec8bbe34230
kelinlang <kelinlang@163.com>
2020-03-02 20:33:44 +0800

    kf

--- Commonlib/src/main/java/com/lib/commonlib/utils/PermssionUtils.java
+++ Commonlib/src/main/java/com/lib/commonlib/utils/PermssionUtils.java
@@ -3,7 +3,9 @@ package com.lib.commonlib.utils;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.support.v4.content.ContextCompat;
+
+import androidx.core.content.ContextCompat;
+/*import android.support.v7.content.ContextCompat;*/
 
 /**
  * Created by dengjun on 2018/4/24.
